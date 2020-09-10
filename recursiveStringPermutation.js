4d37f7a3e4aa6df2170a291d2c27076e3c40596f
yerx <yer.x.xiong@gmail.com>
2020-09-10 08:02:17 -0400

    added two work in progress alternative solutions

--- recursiveStringPermutation.js
+++ recursiveStringPermutation.js
@@ -15,7 +15,7 @@ function getPermutations(string) {
   // put the last char in all possible positions for each of the above permutations
   const permutations = new Set();
   permutationsOfAllCharsExceptLast.forEach(
-    (permutationsOfAllCharsExceptLast) => {
+    (permutationsOfAllCharsExceptLast => {
       for (
         let position = 0;
         position <= allCharsExceptLast.length;
@@ -29,4 +29,45 @@ function getPermutations(string) {
       }
     }
   );
+}
+
+// solution 2 -- WIP
+
+function getPermutations(string) {
+// declare variable set equal to empty array
+  let permutations = new Set();
+
+// if input length is equal to 0, add it to the set
+  if (input.length === 0) {
+    permutations.add(string);
+  }
+
+// iterate through the input
+  for (let i = 0; i < input.length; i++) {
+     // add input at index to string variable
+    string += input[i];
+  // return the function with the arguments of the current string and input that excludes the current character at the index
+    return recursiveStringPermutation(input.slice(0, i).concat(input.slice(i + 1)), string)
+  }
+
+  return permutations;
+
+}
+
+const input1 = 'cats';
+console.log(getPermutations(input1))
+
+// solution 3 - WIP
+function getPermutations(string) {
+  let permutations = new Set();
+
+  (function combinations(str, input) {
+    if (input === '') { permutations.add(str)};
+
+    for (let i = 0; i < input.length; i++) {
+      combinations(str + str[i], input.slice(0, i) + input.slice(i + 1))
+    }
+  })('', string);
+
+  return permutations;
 }
